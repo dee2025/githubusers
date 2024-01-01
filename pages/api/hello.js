10778c54bd83a8b52f8409e19bb24ea7274eabea
deepak singh <108868111+dee2025@users.noreply.github.com>
2024-01-02 00:08:24 +0530

    Add files via upload

---
+++ pages/api/hello.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+
+  const userSlug = req.query.inputValue;
+  try {
+    const response = await fetch(`https://api.github.com/users/${userSlug}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data from GitHub API:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,136 @@
+import { useState } from 'react';
+import { Inter } from 'next/font/google';
+import { IoSend } from "react-icons/io5";
+import { Avatar, Typography, Chip } from "@material-tailwind/react";
+import { FaBuilding, FaGithubSquare } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import Link from 'next/link';
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+
+  const data = [
+    {
+      label: "PROFILE",
+      value: "profile",
+    },
+    {
+      label: "BIO",
+      value: "bio",
+    },
+    {
+      label: "BLOGS",
+      value: "blogs",
+    },
+  ];
+ 
+
+  const [inputValue, setInputValue] = useState('');
+  const [userData, setUserData] = useState(null);
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch(`/api/hello?inputValue=${encodeURIComponent(inputValue)}`);
+      const data = await response.json();
+      setUserData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+
+  console.log(userData)
+
+  return (
+    <main className={`flex flex-col gap-5 h-screen items-center bg-black !font-sans ${inter.className}`} >
+      <div className=' w-[40%] mt-12 rounded-xl py-8 border border-cyan-400 px-8'>
+        <form className='flex justify-between items-center' onSubmit={handleSubmit}>
+          <input
+            className='text-black border w-[72%] py-2 px-4 rounded-lg font-rajdhani font-bold tracking-wide'
+            placeholder='Write Github Profile Username'
+            type='text'
+            name='username'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className='flex text-white bg-cyan-500 justify-center text-2xl py-2 rounded-lg w-[24%]' type='submit'>
+            <IoSend />
+          </button>
+        </form>
+      </div>
+
+      {userData && (
+        <div className='w-[50%] px-8 py-4 mt-8'>
+          <Tabs value="profile">
+            <TabsHeader className="bg-transparent gap-4">
+              {data.map(({ label, value }) => (
+                <Tab className='my-5 text-cyan-800 text-sm font-semibold font-sen border rounded-lg' key={value} value={value}>
+                  {label}
+                </Tab>
+              ))}
+            </TabsHeader>
+            <TabsBody className='border '>
+              {data.map(({ value, desc }) => (
+                <TabPanel key={value} value={value} className="py-0 px-5 mt-5 h-48">
+                  {
+                    value == "profile" ? (
+                      <div>
+                          <div className="flex gap-4">
+                            <Avatar className='h-24 w-24' src={userData.avatar_url} alt="avatar" variant="rounded" />
+                            <div>
+                              <p className='capitalize font-rajdhani font-extrabold text-4xl text-cyan-100'>{userData.name} <span className='text-sm tracking-wider text-white'>[ {userData.login} ]</span> </p>
+                              <div className='flex gap-5 mt-4 '>
+                                <Link href={userData.followers_url} >
+                                  <Chip color='cyan' className='hover:bg-cyan-400 text-[10px] tracking-wider hover:text-white font-sentence hover:duration-1000 hover:transition-colors' size="sm" variant="outlined" value={`${userData.followers} Followers`} />
+                                </Link>
+                                <Link href={userData.following_url} >
+                                  <Chip color="cyan" className='hover:bg-cyan-400 text-[10px] tracking-wider hover:text-white font-sentence hover:duration-1000 hover:transition-colors' size="sm" variant="outlined" value={`${userData.following} Following`} />
+                                </Link>
+                                <Link target='_blank' href={`https://github.com/${userData.login}`}>
+                                  <FaGithubSquare className='text-2xl hover:text-cyan-700 hover:duration-1000 hover:transition-colors' />
+                                </Link>
+                              </div>
+                            </div>
+                          </div>
+                          <div className='mt-4'>
+                            <span className='flex gap-2 items-center text-sm mb-2'>
+                              <FaBuilding className='text-cyan-400'/> <p className='text-green-400 font-sentence'>{userData.company}</p>
+                            </span>
+                            <span className='flex gap-2 items-center text-sm mb-2'>
+                              <FaLocationDot className='text-cyan-500' /> <p className='text-green-400 font-sentence'>{userData.location} </p>
+                            </span>
+                          </div>
+                      </div>
+                    ) : value == "bio" ? (
+                      <div>
+                      <Typography variant="large" className="font-normal py-2 font-sen text-cyan-600">
+                            {userData.bio}
+                      </Typography>
+                    </div>
+                    ) : ("")
+                  }
+                  
+                </TabPanel>
+              ))}
+            </TabsBody>
+          </Tabs>
+          
+          
+        </div>
+            )}
+    </main>
+  );
+}
+
+
+{/*  */}
